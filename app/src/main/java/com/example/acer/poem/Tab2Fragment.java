ee67ca3767b43160adcdab896868cbf1f0b8b2e8
Lindazyy <934174481@qq.com>
2018-05-13 15:09:25 +0800

    update

--- app/src/main/java/com/example/acer/poem/Tab2Fragment.java
+++ app/src/main/java/com/example/acer/poem/Tab2Fragment.java
@@ -1,17 +1,21 @@
 package com.example.acer.poem;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+
+import java.util.List;
 
 public class Tab2Fragment extends android.support.v4.app.Fragment {
+    //The interface to introduce history
+
+    private List <Fragment> catalog; //the list to introduce
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2_layout, container, false);
-        ImageView iv = (ImageView) view.findViewById(R.id.fragment2imag);
-        iv.setImageResource(R.drawable.read);
+        view.setBackgroundResource(R.drawable.tab2_background_resource);
 
         return view;
     }
