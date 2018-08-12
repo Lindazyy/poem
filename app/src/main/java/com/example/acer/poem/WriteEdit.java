ba98ab3133d1a195485c0507abc77abf00b99a3d
allegroxs <allegroxs@sjtu.edu.cn>
2018-08-12 15:18:36 +0800

    Failed to save/insert

--- app/src/main/java/com/example/acer/poem/WriteEdit.java
+++ app/src/main/java/com/example/acer/poem/WriteEdit.java
@@ -5,13 +5,13 @@
 
 package com.example.acer.poem;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -21,17 +21,18 @@ import android.widget.Toast;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class WriteEdit extends Activity implements OnClickListener {
+public class WriteEdit extends AppCompatActivity implements OnClickListener {
 
     private TextView tv_date;
     private TextView tv_cipaiming;
     private EditText et_content;
     private EditText et_title;
-    private Button btn_ok;
+    private Button btn_save;
     private Button btn_cancel;
     private Button btn_check;
     private Button btn_hint;
-    private PoemDataBaseHelper DBHelper;
+    private MyApplication app;
+
     public int enter_state = 0;//用来区分是新建还是更改
     public String last_content;//用来获取内容
 
@@ -39,20 +40,21 @@ public class WriteEdit extends Activity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit);
+        app = (MyApplication) getApplication();
+        app.wDBAdapter.open();
 
         InitView();
     }
 
     private void InitView() {
-        //tv_date = (TextView) findViewById(R.id.tv_date);
+        tv_date = (TextView) findViewById(R.id.tv_date);
         tv_cipaiming = (TextView) findViewById(R.id.tv_cipaiming);
         et_content = (EditText) findViewById(R.id.et_content);
         et_title = (EditText) findViewById(R.id.et_title);
-        btn_ok = (Button) findViewById(R.id.btn_ok);
+        btn_save = (Button) findViewById(R.id.btn_save);
         btn_cancel = (Button) findViewById(R.id.btn_cancel);
         btn_check = (Button) findViewById(R.id.btn_check);
         btn_hint = (Button) findViewById(R.id.btn_hint);
-        DBHelper = new PoemDataBaseHelper(this);
 
         //传递词牌名
         tv_cipaiming.setText("词牌名");
@@ -61,9 +63,9 @@ public class WriteEdit extends Activity implements OnClickListener {
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         String dateString = sdf.format(date);
-        //tv_date.setText(dateString);
+        tv_date.setText(dateString);
 
-/*导致崩溃：
+/*无可接收，导致崩溃：
         //接收内容和id
         Bundle myBundle = this.getIntent().getExtras();
         last_content = myBundle.getString("info");
@@ -72,56 +74,63 @@ public class WriteEdit extends Activity implements OnClickListener {
 */
         //监听
         btn_cancel.setOnClickListener(this);
-        btn_ok.setOnClickListener(this);
+        btn_save.setOnClickListener(this);
         btn_check.setOnClickListener(this);
         btn_hint.setOnClickListener(this);
 
     }
     @Override
     public void onClick(View view) {
+        // 获取EditText内容
+        //SQLiteDatabase db = DBHelper.getReadableDatabase();
+        String title = et_title.getText().toString();
+        String content = et_content.getText().toString();
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String dateString = sdf.format(date);
 
         switch (view.getId()) {
-            case R.id.btn_ok://将结果存入数据库
-/*
-                // 获取edittext内容
-                SQLiteDatabase db = DBHelper.getReadableDatabase();
-                String content = et_content.getText().toString();
+            case R.id.btn_save://将结果存入数据库
+
+                WritingItem writingItem = new WritingItem();
+                writingItem.Title = title;
+                writingItem.Content = content;
+                writingItem.Date = dateString;
 
                 // 添加一个新的日志
                 if (enter_state == 0) {
                     if (!content.equals("")) {
-                        //获取此时时刻时间
-                        Date date = new Date();
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-                        String dateString = sdf.format(date);
-
-                        //向数据库添加信息
+                        app.wDBAdapter.insert(writingItem);
+                        /*//向数据库添加信息
                         ContentValues values = new ContentValues();
                         values.put("content", content);
                         values.put("date", dateString);
-                        db.insert("note", null, values);
+                        db.insert("note", null, values);*/
+                        Toast.makeText(WriteEdit.this, "Saved", Toast.LENGTH_SHORT).show();
                         finish();
                     } else {
-                        Toast.makeText(WriteEdit.this, "请输入", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(WriteEdit.this, "Please input something", Toast.LENGTH_SHORT).show();
                     }
                 }
                 // 查看并修改一个已有的作品
                 else {
-                    ContentValues values = new ContentValues();
-                    values.put("content", content);
-                    db.update("note", values, "content = ?", new String[]{last_content});
+                    app.wDBAdapter.updateOneData(1, writingItem);
                     finish();
                 }
-*/
+
                 break;
 
             case R.id.btn_cancel:
-                Builder dialog = new AlertDialog.Builder(WriteEdit.this);
-                dialog.setTitle("Exit without saving?");
-                dialog.setPositiveButton("Yes",new okClick());
-                dialog.setNegativeButton("No",new noClick());
-                dialog.create();
-                dialog.show();
+                if (!content.equals("")) {
+                    Builder dialog = new AlertDialog.Builder(WriteEdit.this);
+                    dialog.setTitle("Exit without saving?");
+                    dialog.setPositiveButton("Yes", new okClick());
+                    dialog.setNegativeButton("No", new noClick());
+                    dialog.create();
+                    dialog.show();
+                }else {
+                    finish();
+                }
                 //finish();
                 break;
 
